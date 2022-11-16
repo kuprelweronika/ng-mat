893235123c1e82c38c977632f627a1b0d40269f9
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2022-11-16 15:17:02 +0100

    feat: starter pack added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductDetailComponentModule } from './components/product-detail/product-detail.component-module';
+import { ProductServiceModule } from './services/product.service-module';
+
+@NgModule({
+  imports: [RouterModule.forRoot([{ path: 'product/:id', component: ProductDetailComponent }]), ProductDetailComponentModule, ProductServiceModule],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import {ProductModel} from "../../models/product.model";
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-product-detail',
+  styleUrls: ['./product-detail.component.scss'],
+  templateUrl: './product-detail.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductDetailComponent {
+  readonly productDetails$: Observable<ProductModel> = this._activatedRoute.params.pipe(
+    switchMap(data => this._productService.getOne(data['id'])));
+
+  constructor(private _activatedRoute: ActivatedRoute, private _productService: ProductService) {
+  }
+}
