d8a634f8df4ecea44f83b8a7d519dbc201f331c4
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2022-11-19 19:27:30 +0100

    feat: product details in grid added

--- src/app/components/products/products.component-module.ts
+++ src/app/components/products/products.component-module.ts
@@ -1,12 +1,13 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
-import {ProductsComponent} from './products.component';
-import {MatButtonModule} from "@angular/material/button";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ProductsComponent } from './products.component';
 
 @NgModule({
-  imports: [MatCardModule, MatListModule, CommonModule, MatButtonModule],
+  imports: [MatCardModule, MatListModule, CommonModule, MatButtonModule, MatGridListModule],
   declarations: [ProductsComponent],
   providers: [],
   exports: [ProductsComponent]
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
-import {BehaviorSubject, Observable, switchMap} from "rxjs";
+import {Observable, Subject, switchMap} from "rxjs";
 import {ProductModel} from "../../models/product.model";
 import {ProductService} from "../../services/product.service";
 
@@ -10,19 +10,31 @@ import {ProductService} from "../../services/product.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsComponent {
+  //for remove and refresh list
+  // private _refreshSubject: BehaviorSubject<void> = new BehaviorSubject<void>(void 0)
+  // public refresh$: Observable<void> = this._refreshSubject.asObservable();
+  // // @ts-ignore
+  // readonly refreshedList$: Observable<ProductModel[]> = this.refresh$.pipe(
+  //   switchMap(data => this._productService.getAll())
+  // );
+  // delete(id: number): void {
+  //   this._productService.delete(id).subscribe()
+  // }
+
+  //selecting product by ID
 
   readonly list$: Observable<ProductModel[]> = this._productService.getAll()
-  private _refreshSubject: BehaviorSubject<void> = new BehaviorSubject<void>(void 0)
-  public refresh$: Observable<void> = this._refreshSubject.asObservable();
-  // @ts-ignore
-  readonly refreshedList$: Observable<ProductModel[]> = this.refresh$.pipe(
-    switchMap(data => this._productService.getAll())
-  );
+  private _selectedProductIdSubject: Subject<string> = new Subject<string>();
+  public selectedProductId$: Observable<string> = this._selectedProductIdSubject.asObservable()
+  readonly details$: Observable<ProductModel> = this.selectedProductId$.pipe(
+    switchMap(data => this._productService.getOne(data))
+  )
 
   constructor(private _productService: ProductService) {
   }
 
-  delete(id: number): void {
-    this._productService.delete(id).subscribe()
+  selectProduct(id: number): void {
+    this._selectedProductIdSubject.next(String(id))
   }
+
 }
